dca4f63d93f42a9efe37b007bc76e2d866927166
Kamil Mosek <kamil.mosek@gmail.com>
2021-10-01 11:12:25 +0200

    add openapi

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
+    path('openapi/', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
