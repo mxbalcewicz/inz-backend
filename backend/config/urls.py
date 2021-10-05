defd2b0cd67dee18c02e30ba3faef23dab2b6319
Kamil Mosek <kamil.mosek@gmail.com>
2021-10-05 11:40:49 +0200

    add api.urls to config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,6 +6,7 @@ from rest_framework.schemas import get_schema_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
+    path('api/', include('api.urls')),
     path('openapi/', get_schema_view(
         title="Your Project",
         description="API for all things …",
