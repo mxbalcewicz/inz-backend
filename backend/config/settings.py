3c62d25cfa8471202ceb16bf13a3ab7e4a1fb9ff
Kamil Mosek <kamil.mosek@gmail.com>
2021-10-27 20:05:16 +0200

    change cors settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,7 +142,9 @@ STATIC_URL = '/static/'
 # CORS SETTINGS
 CORS_ALLOWED_ORIGINS = [
     'https://localhost:3000',
-    'https://127.0.0.1:3000'
+    'http://localhost:3000',
+    'https://127.0.0.1:3000',
+    'http://127.0.0.1:3000'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
